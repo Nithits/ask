502e9e266e3b9e714ddece46fa6b900d93a4974f
Nithits <nithisriwang2546@gmail.com>
2026-04-10 04:35:17 +0700

    test edit code

--- www/admin/dashboard.php
+++ www/admin/dashboard.php
@@ -228,6 +228,7 @@ try {
     </div>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
 // 1. วาดกราฟสถิติ (Chart.js)
